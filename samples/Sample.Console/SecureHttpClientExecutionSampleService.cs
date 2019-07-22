5783fa0b39ec06f261f0c96c312d0ab510d6d8c8
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-07-22 17:32:27 +0300

    Update solution structure & fix pipelines

--- samples/Sample.Console/SecureHttpClientExecutionSampleService.cs
+++ samples/Sample.Console/SecureHttpClientExecutionSampleService.cs
@@ -1,31 +1,31 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Softeq.HttpClient.Common;
-using Softeq.HttpClient.Common.Executor;
+using Softeq.XToolkit.CrossCutting;
+using Softeq.XToolkit.CrossCutting.Executor;
 using Softeq.XToolkit.DefaultAuthorization;
+using Softeq.XToolkit.DefaultAuthorization.Abstract;
 using Softeq.XToolkit.DefaultAuthorization.Extensions;
 
-namespace Softeq.Sample
+namespace Sample.Console
 {
     public class SecureHttpClientExecutionSampleService
     {
-        private readonly SecuredTokenManager _tokenManager;
+        private readonly ISecuredTokenManager _tokenManager;
         private readonly SessionApiService _sessionApiService;
         private readonly SecuredHttpServiceGate _http;
 
         public SecureHttpClientExecutionSampleService()
         {
-            var testAuthConfig =
-                new AuthConfig("http://lilbytes-softeq-test.azurewebsites.net", "ro.client", "secret");
+            var testAuthConfig = new AuthConfig("http://qwerty.azurewebsites.net", "ro.client", "secret");
             var httpConfig = new HttpServiceGateConfig();
-            _tokenManager = new SecuredTokenManager();
+            _tokenManager = new ConsoleSecuredTokenManager();
             _sessionApiService = new SessionApiService(testAuthConfig, httpConfig, _tokenManager);
             _http = new SecuredHttpServiceGate(_sessionApiService, httpConfig, _tokenManager);
         }
 
         public async Task LoginAsync()
         {
-            await _sessionApiService.LoginAsync("user@test.com", "123QWqw1");
+            await _sessionApiService.LoginAsync("login", "pass");
         }
 
         public async Task<ExecutionResult<string>> MakeRequestWithCredentials()
@@ -40,7 +40,7 @@ namespace Softeq.Sample
                     .SetMethod(HttpMethods.Get)
                     .WithCredentials(_tokenManager);
 
-                    var response = await _http.ExecuteApiCallAsync(HttpRequestPriority.High, request);
+                    var response = await _http.ExecuteApiCallAsync(request, priority: HttpRequestPriority.High);
 
                     if (response.IsSuccessful)
                     {
